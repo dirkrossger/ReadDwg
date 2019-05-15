b82b2b6ceb35bdf82f939fd58df0179ec685a6f3
dirkrossger <dirk.roessger@gmail.com>
2019-05-15 07:21:18 +0200

    19-05-15

---
+++ ListItems/Commands.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#region Autodesk libraries
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+#endregion
+
+[assembly: CommandClass(typeof(ListItems.Commands))]
+
+namespace ListItems
+{
+    class Commands
+    {
+        [CommandMethod("DisplayLayers", "displa", CommandFlags.Modal)]
+        public void DisplayLayers()
+        {
+            Document doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
+            List<string> info = LayersToList(db);
+            foreach (string lname in info)
+                ed.WriteMessage("\nLayer Name: \t{0}", lname);
+
+        }
+
+        [CommandMethod("WriteLayers", "writela", CommandFlags.Modal)]
+        public void WriteLayersToFile()
+        {
+            Document doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
+            List<string> info = LayersToList(db);
+
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"C:\temp\WriteLayers.txt"))
+            {
+                foreach (string lname in info)
+                {
+                    file.WriteLine(lname);
+                }
+            }
+        }
+
+
+        public List<string> LayersToList(Database db)
+        {
+            List<string> lstlay = new List<string>();
+
+            LayerTableRecord layer;
+            using (Transaction tr = db.TransactionManager.StartOpenCloseTransaction())
+            {
+                LayerTable lt = tr.GetObject(db.LayerTableId, OpenMode.ForRead) as LayerTable;
+                foreach (ObjectId layerId in lt)
+                {
+                    layer = tr.GetObject(layerId, OpenMode.ForWrite) as LayerTableRecord;
+                    lstlay.Add(layer.Name);
+                }
+
+            }
+            return lstlay;
+        }
+    }
+}
+
+
--- Readme.txt
+++ Readme.txt
@@ -1,2 +1,5 @@
 //Opportunity to read Dwg
 https://forums.autodesk.com/t5/net/how-to-pass-parameter-in-interface-to-the-programe-of-dll-file/td-p/3371819
+
+19-05-15
+- list items from Acad
