cf13e76672ae398f6841d24359acdb20176ca33c
Dirk Rössger <dirk.roessger@gmail.com>
2019-06-03 20:10:07 +0200

    190603_ObjDbx_ReadLayers

--- ObjDbx/Program.cs
+++ ObjDbx/Program.cs
@@ -8,6 +8,22 @@ using AutoCAD;
 
 namespace ObjDbx
 {
+    //'=================================================================================================
+    //' Purpose   : Create an object class for access Autocad via the ObjectDBX
+    //'
+    //' Date      : 01/19/18
+    //' Updated   :
+    //'=================================================================================================
+    //' Need code to differentiate the various versions of ACAD?
+    //' 2018-01-19
+    //' Replaced AXDBLib with AXDBLib
+    //'=================================================================================================
+    //' References Requires
+    //' AutoCAD    C:\Program Files\Common Files\Autodesk Shared\acax22enu.tlb
+    //' AXDBLib    C:\Program Files\Common Files\Autodesk Shared\axdb22enu.tlb
+    //' Scripting  C:\Windows\SysWOW64\scrrun.dll
+    //'=================================================================================================
+
     class Program
     {
         static void Main(string[] args)
@@ -17,24 +33,41 @@ namespace ObjDbx
             Console.WriteLine("AutoCAD started!");
 
             //Open an AxDbDocument
-            object obj = acad.GetInterfaceObject("ObjectDBX.AxDbDocument.17"); //Acad2018
+            object obj = acad.GetInterfaceObject("ObjectDBX.AxDbDocument.22"); //Acad2018
             AxDbDocument doc = obj as AxDbDocument;
-
 
             if (doc == null)
             {
                 Console.WriteLine("Cannot open AxDbDocument for some reason!");
                 return;
             }
+            else
+            {
+                doc.Open(@"c:\Users\Dick\OneDrive - Integra Engineering AB\Autocad\Infratools\M-Stomlinje.dwg");
+                List<string> layers = AcLayersToList(doc.Database);
+                Console.WriteLine(layers.Count + " Layers found.");
+                foreach (string s in layers)
+                    Console.WriteLine(s);
 
-
+            }
             //Get drawing information
-            Console.WriteLine("Entities in ModelSpace: " + doc.ModelSpace.Count);
+            //Console.WriteLine("Entities in ModelSpace: " + doc.ModelSpace.Count);
 
             //You can make changes to the entities in AxDbDocument and save the changes back to the drawing file.
 
+            //doc.Save();
+            Console.Read();
+        }
 
-            doc.Save();
+        private static List<string> AcLayersToList(AXDBLib.AcadDatabase database)
+        {
+            List<string> lstlay = new List<string>();
+
+            foreach(AXDBLib.AcadLayer lay in database.Layers)
+            {
+                lstlay.Add(lay.Name);
+            }
+            return lstlay;
         }
     }
 }
